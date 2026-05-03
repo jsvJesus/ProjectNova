aa286c36658fcb5b45bd620c77aa99cdb841613f
jsvJesus <es3n1n.eng@gmail.com>
2026-05-04 00:13:38 +0300

    Stage 03: Add interaction system and world item pickup

--- Source/ProjectNova/Private/Interaction/PNInteractableInterface.cpp
+++ Source/ProjectNova/Private/Interaction/PNInteractableInterface.cpp
@@ -1,13 +1,1 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "Interaction/PNInteractableInterface.h"
-
-
-PNInteractableInterface::PNInteractableInterface()
-{
-}
-
-PNInteractableInterface::~PNInteractableInterface()
-{
-}
